0d195ea62ea64ead180c4b4d702e214bebd8e9ae
exitdump <jahidhasan100101@gmail.com>
2025-03-04 15:53:14 +0600

    Add switch component to Dashboard for toggling state

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,7 +1,18 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Switch } from '@/components/ui/switch';
 import { Head } from '@inertiajs/react';
+import { useState } from 'react';
 
 export default function Dashboard() {
+     // Initialize the switch state
+  const [isChecked, setIsChecked] = useState(false);
+
+  // Handler for when the switch state changes
+  const handleCheckedChange = (checked) => {
+    console.log("Switch is now:", checked);
+    setIsChecked(checked);
+  };
+
     return (
         <AuthenticatedLayout
             header={
@@ -16,8 +27,14 @@ export default function Dashboard() {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            You're logged in!
+                            You're logged in! {isChecked ? "Switch is on" : "Switch is off"}
                         </div>
+                        <Switch
+                         checked={isChecked} 
+                         onCheckedChange={handleCheckedChange}
+                         >
+                            CLick
+                        </Switch>
                     </div>
                 </div>
             </div>
